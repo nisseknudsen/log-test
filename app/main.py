a694cbbafc18b4d36cff5f78f4eb541114c5a069
Nisse Knudsen <nisse.knudsen@gmail.com>
2025-04-02 16:39:34 -0700

    Refactor main.py to implement logging and datetime functionality; remove outbound topic configuration from MAKE87.yml

--- app/main.py
+++ app/main.py
@@ -1,19 +1,36 @@
 import time
+import sys
 
 from make87_messages.core.header_pb2 import Header
 from make87_messages.text.text_plain_pb2 import PlainText
 import make87
+from datetime import datetime, timezone
+
+import logging
+
+
+def get_current_datetime():
+    return datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z")
 
 
 def main():
     make87.initialize()
-    topic = make87.get_publisher(name="HELLO_WORLD_MESSAGE", message_type=PlainText)
 
     while True:
-        message = PlainText(header=make87.create_header(Header, entity_path="/"), body="Hello, World! 🐍")
-        topic.publish(message)
-        print(f"Published: {message}")
-        time.sleep(1)
+        logging.info(f"Info @ {get_current_datetime()}")
+        time.sleep(0.1)
+        logging.warning(f"Warning @ {get_current_datetime()}")
+        time.sleep(0.1)
+        logging.error(f"Error @ {get_current_datetime()}")
+        time.sleep(0.1)
+        logging.debug(f"Debug @ {get_current_datetime()}")
+        time.sleep(0.1)
+        logging.critical(f"Critical @ {get_current_datetime()}")
+        time.sleep(0.1)
+        print(f"Stdout Print @ {get_current_datetime()}", file=sys.stdout)
+        time.sleep(0.1)
+        print(f"Stderr Print @ {get_current_datetime()}", file=sys.stderr)
+        time.sleep(5.0)
 
 
 if __name__ == "__main__":
