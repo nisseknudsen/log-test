5d4b40b55d594a0ced25b8f122594b76418730a5
todo@make87.com <make87>
2025-04-08 20:52:13 +0000

    Reordered log calls and set level to "DEBUG"

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 import time
 import sys
 
-from make87_messages.core.header_pb2 import Header
-from make87_messages.text.text_plain_pb2 import PlainText
 import make87
 from datetime import datetime, timezone
 
 import logging
-
+logging.basicConfig(level=logging.DEBUG)
 
 def get_current_datetime():
     return datetime.now(timezone.utc).isoformat(timespec="milliseconds").replace("+00:00", "Z")
@@ -17,13 +15,13 @@ def main():
     make87.initialize()
 
     while True:
+        logging.debug(f"Debug @ {get_current_datetime()}")
+        time.sleep(0.1)
         logging.info(f"Info @ {get_current_datetime()}")
         time.sleep(0.1)
         logging.warning(f"Warning @ {get_current_datetime()}")
         time.sleep(0.1)
         logging.error(f"Error @ {get_current_datetime()}")
-        time.sleep(0.1)
-        logging.debug(f"Debug @ {get_current_datetime()}")
         time.sleep(0.1)
         logging.critical(f"Critical @ {get_current_datetime()}")
         time.sleep(0.1)
